c0c2e98b4e97700bc6709cf74ce21ce1926d6659
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2025-01-02 23:45:50 -0500

    this is prob why im gonna be homeless

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,17 +1,33 @@
 import './App.css';
 import {Task} from './components/Task';
 import {useState} from 'react';
+import audio from "./music/oo-ee-a-ea.mp3";
+import surpriseaudio from "./music/bye-bye.mp3";
 
 function App() {
 
-  const [tasksList, addTask] = useState(
-    []
-  );
+  const playAudio = async() => {
+    const imsorry = new Audio(audio);
+    imsorry.preload = "auto";
+    await imsorry.play();
+  };
 
-  let [input, curInput] = useState("");
+  const surpriseAudio = async() => {
+    const imsorry = new Audio(surpriseaudio);
+    imsorry.preload = "auto";
+    await imsorry.play(); 
+    
+  }
+
+  const [tasksList, addTask] = useState([]);
+  const [input, curInput] = useState("");
+  let [amogus, showAmogus] = useState(false);
 
   return (
     <div className="App">
+      {amogus === true && <div className="amogus"></div>}
+      <div className="Useful">
+      <div className="Left">
       <div className="Text">
         <h1 className="Headline">TO-DO LIST</h1>
         <h1 className="Subheading">DESTROYER OF ALL THAT IS PROCRASTINATION</h1>
@@ -19,19 +35,32 @@ function App() {
       <div className="Content">
         <h1>{ tasksList.map((task, key) => { 
           return(
-          <div>
-            <button className="Complete" onClick={() => { 
+          <div className="tasks">
+            <button id="Complete" onClick={() => { 
               const newTaskList = tasksList.map((ntask, i) => i === key ? {...ntask, status: !ntask.status } : ntask); 
               addTask(newTaskList); 
+              
             }
-          }>Complete</button> 
+          }> </button> 
           <Task task={task.task} status={task.status}/></div>)
           } ) }</h1>
         <input id="Input" type="text" value={input} onInput={(e) => { curInput(e.target.value); }}></input>
+        <br></br>
         <button id="Add" onClick={() => { 
           curInput(""); 
           addTask([...tasksList, {task: input, status: false}]); 
+          playAudio();
           }}>ADD TASK</button>
+        {/* <button id="Brainrotify" onClick={() => { showAmogus(amogus=!amogus); if(amogus) { surpriseAudio(); } }}>you know you want to click me 😏 oh you want to so bad</button> */}
+      </div>
+      </div>
+      <div className="Right">
+      {amogus === true && <div className="mog"></div>}
+
+      </div>
+      </div>
+      <div className="Surprise">
+        {amogus === true && <div className="amogus"></div>}
       </div>
     </div>
   );
--- todolist/src/components/Task.jsx
+++ todolist/src/components/Task.jsx
@@ -1,6 +1,5 @@
 export const Task = (props) => {
     return (
-        props.status ? ( <h1 style={{textDecoration: "line-through"}}>{props.task}</h1>) : (<h1>{props.task}</h1>)
-
+         props.status ? ( <h1 className="word"style={{textDecoration: "line-through"}}>{props.task}</h1>) : (<h1 className="word">{props.task}</h1>) 
     );
 }
